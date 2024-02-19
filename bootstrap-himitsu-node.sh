90a36106b02df0820c34426a731b80f3818e8022
HimitsuCTO <158521076+HimitsuCTO@users.noreply.github.com>
2024-02-19 12:18:22 -0800

    Update bootstrap-himitsu-node.sh

--- bootstrap-himitsu-node.sh
+++ bootstrap-himitsu-node.sh
@@ -31,7 +31,7 @@ git clone https://github.com/HimitsuCTO/himitsu
 #cd into the repo
 cd himitsu
 
-ignite chain build
+make install
 
 #himitsud init [moniker] 
 himitsud init myhimitsunode
@@ -46,7 +46,7 @@ sed -i '/\[api\]/,+6 s/swagger = false/swagger = true/' ~/.himitsu/config/app.to
 
 sed -i 's/laddr = "tcp:\/\/127\.0\.0\.1:26657"/laddr = "tcp:\/\/0.0.0.0:26657"/' ~/.himitsu/config/config.toml
 sed -i 's/address = "tcp:\/\/localhost:1317"/address = "tcp:\/\/0.0.0.0:56789"/' ~/.himitsu/config/app.toml
-sed -i 's/^persistent_peers = .*/persistent_peers = "10369aeaabba1e88167caa6e1008b4c1a7c6a820@172.233.157.235:26656@a96598f601e4d5b8863cf0cd1a0bcf560a86d27d@194.195.122.49:26656"/' ~/.himitsu/config/config.toml
+sed -i 's/^persistent_peers = .*/persistent_peers = "10369aeaabba1e88167caa6e1008b4c1a7c6a820@172.233.157.235:26656,a96598f601e4d5b8863cf0cd1a0bcf560a86d27d@194.195.122.49:26656"/' ~/.himitsu/config/config.toml
 
-sed -i 's/^seeds = .*/seeds = "10369aeaabba1e88167caa6e1008b4c1a7c6a820@172.233.157.235:26656@a96598f601e4d5b8863cf0cd1a0bcf560a86d27d@194.195.122.49:26656"/' ~/.himitsu/config/config.toml
+sed -i 's/^seeds = .*/seeds = "10369aeaabba1e88167caa6e1008b4c1a7c6a820@172.233.157.235:26656,a96598f601e4d5b8863cf0cd1a0bcf560a86d27d@194.195.122.49:26656"/' ~/.himitsu/config/config.toml
 himitsud start 
